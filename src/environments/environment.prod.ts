a35d93939f650764361549087e6257d1930121b8
Vedant <vedantarole@rediffmail.com>
2022-09-01 12:24:12 +0530

    production commt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://rawg-video-games-database.p.rapidopi.com'
+  BASE_URL: 'https://rawg-video-games-database.p.rapidopi.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAHS6qmVP19bQK_LSvb6mp5wZfD0jlf8NA',
+    authDomain: 'customerapp-7e542.firebaseapp.com',
+    projectId: 'customerapp-7e542',
+    storageBucket: 'customerapp-7e542.appspot.com',
+    messagingSenderId: '206931647959',
+    appId: '1:206931647959:web:e6efbf180042268b7a7f92',
+  },
 };
